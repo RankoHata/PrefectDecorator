b56f0c5331d49a92c6c9756aae7d3208b3052414
RankoHata <41975337+RankoHata@users.noreply.github.com>
2018-12-20 12:43:11 +0800

    Fix bugs.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PrefectDecorator',
-    version='0.0.1',
+    version='0.0.2',
     description='Some convenient decorators.',
     author='RankoHata',
     author_email='san332627946@gmail.com',
